bea3abcaff58c33b7c36ae40d0ae175c2070be96
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-09-24 23:10:39 -0300

    correct title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({ score, highScore }) {
   return (
     <header className="select-none text-white flex flex-col items-center">
       <h1 className=" font-pokeSolid text-6xl line  tracking-wider mb-10 text-center">
-        PokeMem
+        PokéMem
       </h1>
       <div className="min-w-[20rem] items-center sm:items-center sm:flex-row sm:min-w-[40rem] font-light flex flex-col bg-cyan-800 sm:justify-between text-xl mb-3 p-3 rounded-md">
         <h4 className="flex gap-2 items-center">
