e028a0844431d6d312706fede26166bda6372f27
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-09-24 21:42:29 -0300

    finish implementing basic logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,71 @@
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
+import {
+  randomValue,
+  shuffleArray,
+  createNumberArray,
+  getRandomValuesFromArray,
+} from "./utils/utils";
+import sucessAudio from "./sounds/onGoodChoice.mp3";
+import lossAudio from "./sounds/onLoss.mp3";
+import highScoreAudio from "./sounds/onBestScore.mp3";
+
+let idPool = createNumberArray(1000);
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(6);
+  const [clickedPokemonIds, setClickedPokemonIds] = useState([]);
+  const [gameOver, setGameOver] = useState(true);
 
-  const generateRandomValue = () => {
-    return Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  const successA = new Audio(sucessAudio);
+  const lossA = new Audio(lossAudio);
+  const highScoreA = new Audio(highScoreAudio);
+
+  const playSuccessAudio = () => {
+    successA.play();
   };
-
-  // To give it more playtime, we could push 12 more pokemons to array every time score gets to 12???
-  useEffect(() => {
-    let numArray = [];
-    for (let i = 0; i < 12; i++) {
-      numArray = [...numArray, generateRandomValue()];
+  const playLossAudio = () => {
+    lossA.play();
+  };
+  const playHighScoreAudio = () => {
+    highScoreA.play();
+  };
+  function handleCardClick(clickedId) {
+    if (!clickedPokemonIds.includes(clickedId)) {
+      setClickedPokemonIds((prevClickedPokemonIds) => [
+        ...prevClickedPokemonIds,
+        clickedId,
+      ]);
+      setScore((prevScore) => prevScore + 1);
+      if (score >= highScore) {
+        playHighScoreAudio();
+      } else {
+        playSuccessAudio();
+      }
+    } else {
+      playLossAudio();
+      setScore(0);
+      setClickedPokemonIds([]);
+      setGameOver(true);
     }
+  }
+
+  useEffect(() => {
+    console.log(pokemonList);
+  }, [pokemonList]);
+
+  useEffect(() => {
+    if (score >= highScore) {
+      setHighScore(score);
+    }
+  }, [score, highScore]);
+
+  useEffect(() => {
+    let [numArray, remaining] = getRandomValuesFromArray(12, idPool);
+    idPool = remaining;
 
     async function fetchData() {
       try {
@@ -34,21 +85,60 @@ function App() {
         console.log(error);
       }
     }
-    fetchData();
-  }, []);
+    if (gameOver) {
+      fetchData();
+      setGameOver(false);
+    }
+  }, [gameOver]);
+
+  useEffect(() => {
+    if (score !== 0 && score >= 11 && score % 11 === 0) {
+      let [numArray, remaining] = getRandomValuesFromArray(12, idPool);
+      idPool = remaining;
+
+      async function fetchData() {
+        try {
+          const pokemonPromises = numArray.map(async (num) => {
+            const pokeResponse = await fetch(
+              `https://pokeapi.co/api/v2/pokemon/${num}`
+            );
+            if (!pokeResponse.ok) {
+              throw new Error(`Failed to fetch Pokemon with id: ${num}`);
+            }
+            const pokemonData = await pokeResponse.json();
+            return pokemonData;
+          });
+          const pokemonListData = await Promise.all(pokemonPromises);
+
+          setPokemonList((prevPokemonList) => [
+            ...prevPokemonList,
+            ...pokemonListData,
+          ]);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      fetchData();
+    }
+  }, [score]);
+
+  const pokemonCardElements = pokemonList.map((p) => (
+    <Card
+      id={p.id}
+      key={p.id}
+      name={p.name}
+      img={p.sprites.other["official-artwork"]["front_default"]}
+      handleCardClick={handleCardClick}
+    />
+  ));
 
   return (
     <main className="p-5 h-full min-h-screen  bg-gradient-to-bl from-cyan-800 to-cyan-950  flex flex-col ">
-      <Header />
+      <Header score={score} highScore={highScore} />
 
       <div className="grid grid-cols-1 grid-rows-2  gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        {pokemonList.map((p) => (
-          <Card
-            key={p.id}
-            name={p.name}
-            img={p.sprites.other["official-artwork"]["front_default"]}
-          />
-        ))}
+        {shuffleArray(pokemonCardElements).slice(0, 12)}
       </div>
     </main>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,23 @@
 import PropTypes from "prop-types";
-import sucessAudio from "../sounds/onGoodChoice.mp3";
-import lossAudio from "../sounds/onLoss.mp3";
 
 Card.propTypes = {
   name: PropTypes.string,
   img: PropTypes.string,
   id: PropTypes.number,
+  handleCardClick: PropTypes.func,
 };
 
-export default function Card({ id, name, img }) {
-  let success = new Audio(sucessAudio);
-  let loss = new Audio(lossAudio);
-  const playSuccessAudio = () => {
-    success.play();
-  };
-
-  const playLossAudio= ()=>{
-    loss.play();
-  }
-
+export default function Card({ id, name, img, handleCardClick }) {
   return (
     <article
-      onClick={playSuccessAudio}
-      className="cursor-pointer transition-colors shadow-md shadow-green-700/50 bg-green-800 hover:bg-green-600 hover:shadow-green-300/50 rounded-lg p-5 flex flex-col items-center 	"
+      onClick={() => handleCardClick(id)}
+      className="select-none cursor-pointer transition-colors shadow-md shadow-green-700/50 bg-green-800 hover:bg-green-600 hover:shadow-green-300/50 rounded-lg p-5 flex flex-col items-center 	"
     >
-      <img className="" src={img} alt={`Picture of the pokemon '${name}'`} />
+      <img
+        className="select-none"
+        src={img}
+        alt={`Picture of the pokemon '${name}'`}
+      />
       <div className="bg-green-950 text-center w-full rounded-md py-1">
         <h1 className="capitalize text-white text-xl font-semibold">{name}</h1>
       </div>
